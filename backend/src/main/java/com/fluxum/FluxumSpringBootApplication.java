44f7f6e8e1550aed013367018d72ef2c0772aba9
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-05-24 21:44:00 -0300

    Basic spring structure created and configured

---
+++ backend/src/main/java/com/fluxum/FluxumSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fluxum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxumSpringBootApplication
+{
+	static void main() 
+    {
+		SpringApplication.run( FluxumSpringBootApplication.class );
+	}
+}
