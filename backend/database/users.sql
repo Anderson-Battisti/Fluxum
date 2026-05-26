3390c7ccf8f090a16ca9801d8659966eabe6127b
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-05-25 22:34:14 -0300

    Users table ddl creation command added and login screen background image added as resource

---
+++ backend/database/users.sql
@@ -0,0 +1,10 @@
+create table users
+(
+	id 						 bigint auto_increment primary key,
+	name                     varchar(100) not null,
+	email                    varchar(254) not null unique,
+	password                 varchar(255) not null,
+	active boolean 			 not null default true,
+	created_at				 datetime not null default current_timestamp,
+	updated_at 				 datetime not null default current_timestamp on update current_timestamp
+);
