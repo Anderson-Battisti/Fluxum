3b212776b5fecd7202eeb0e6d58126a1cc051bfb
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-05-25 22:57:03 -0300

    User entity created/defined in package model to reflect the users table on database

---
+++ backend/src/main/java/com/fluxum/model/User.java
@@ -0,0 +1,41 @@
+package com.fluxum.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class User
+{
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+    
+    @NotEmpty
+    @Size( max = 100 )
+    @Column( nullable = false, length = 100 )
+    private String name;
+    
+    @NotEmpty
+    @Size( max = 254 )
+    @Column( nullable = false, length = 254, unique = true )
+    private String email;
+    
+    @NotEmpty
+    @Column( nullable = false, length = 255 )
+    private String password;
+    
+    @Column( nullable = false )
+    private Boolean active = true;
+    
+    @Column( nullable = false, updatable = false )
+    private LocalDateTime createdAt;
+    
+    @Column( nullable = false )
+    private LocalDateTime updatedAt;
+}
