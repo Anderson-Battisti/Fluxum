eeb54d27f91eee09bfbd2d9a9cf482ffac5531ef
Anderson Battisti <anderson.battisti@universo.univates.br>
2026-05-27 23:00:19 -0300

    created LoginScreenComponents structure, created the login card div and configured the theme.css/colors variables, also created the translation mechanism structure

---
+++ frontend/src/components/LoginScreenComponents/LoginCard/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.css';
+
+export function LoginCard()
+{
+    return (
+        <div className={ styles.container } >
+            {/*<LoginCardHeader />
+            <EmailInputField />
+            <LoginButton />
+            <Separator />
+            <GoogleLoginButton />
+            <FooterInfo />*/}
+        </div>
+    )
+}
--- frontend/src/pages/LoginScreen/index.tsx
+++ frontend/src/pages/LoginScreen/index.tsx
@@ -1,10 +1,11 @@
+import { LoginCard } from '../../components/LoginScreenComponents/LoginCard';
 import styles from './styles.module.css';
 
 export function LoginScreen()
 {
     return (
         <div className={ styles.container } >
-
+            <LoginCard />
         </div>
     );
 }
